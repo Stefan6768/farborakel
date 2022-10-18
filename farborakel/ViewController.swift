8fa3a4f9f848b5095c8868376a56aaa2f5d7f544
Stefan <103416259+Stefan6768@users.noreply.github.com>
2022-10-18 15:47:22 +0200

    Hintergrund Farbwechsel funktioniert

--- farborakel/ViewController.swift
+++ farborakel/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonbackground: UIButton!
+    
+    @IBOutlet weak var buttoncircle: UIButton!
+    
+    @IBOutlet weak var circle: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func randomcolor() -> UIColor{
+            let red: CGFloat = CGFloat(drand48())
+            let green: CGFloat = CGFloat(drand48())
+            let blue: CGFloat = CGFloat(drand48())
+            return UIColor(red:red, green:green, blue:blue, alpha: 1.0)
+        }
+    @IBAction func changebackground(_ sender:Any) {
+        self.view.backgroundColor = randomcolor()
+    }
+    
+    @IBAction func changecircle(_ sender:Any) {
+        self.view.backgroundColor = randomcolor()
+    }
 
 }
 
